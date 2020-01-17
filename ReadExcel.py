3fcb9d983085a995a04863910088e5420185005a
pli888 <peter@gigasciencejournal.com>
2020-01-17 16:59:23 +0800

    Add Python and test Excel files

---
+++ ReadExcel.py
@@ -0,0 +1,37 @@
+import xlrd
+import os.path
+
+# ----------------------------------------------------------------------
+def open_file(path):
+    """
+    Open and read an Excel file
+    """
+    book = xlrd.open_workbook(path)
+
+    # print number of sheets
+    print(book.nsheets)
+
+    # print sheet names
+    print(book.sheet_names())
+
+    # get the first worksheet
+    first_sheet = book.sheet_by_index(0)
+
+    # read a row
+    print(first_sheet.row_values(0))
+
+    # read a cell
+    cell = first_sheet.cell(0, 0)
+    print(cell)
+    print(cell.value)
+
+    # read a row slice
+    print(first_sheet.row_slice(rowx=0, start_colx=0, end_colx=2))
+
+# ----------------------------------------------------------------------
+if __name__ == "__main__":
+    path = "sep2-2019.xls"
+
+    if os.path.exists(path):
+        print("File exists!")
+        open_file(path)
